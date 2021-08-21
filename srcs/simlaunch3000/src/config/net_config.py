c10f093a406fa35b144354a1d01520a854363b8f
Joep Barmentlo <JoepB@example.com>
2021-08-21 20:36:40 +0200

    simlaunch

--- srcs/simlaunch3000/src/config/net_config.py
+++ srcs/simlaunch3000/src/config/net_config.py
@@ -4,7 +4,7 @@ net_config = DotDict()
 net_config.host = 'localhost'
 net_config.start_port = 9093
 
-net_config.server_port = 9081
+net_config.server_port = 9082
 net_config.server_refresh_time = 1 # seconds to wait before listening again
 
 net_config.start_sim_request = "start"
