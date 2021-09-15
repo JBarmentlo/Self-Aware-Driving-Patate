01c36becf7ee294f82dc6f2db79bbd014fa8fbec
Joep Barmentlo <JoepB@example.com>
2021-09-15 13:57:47 +0200

    gonna optimize replay memory

--- srcs/config.py
+++ srcs/config.py
@@ -185,15 +185,15 @@ if (agent_type == "DQN"):
     config_Agent.data               = config_NeuralPlayer.config_Datasets.ddqn
     config_Agent.action_space_size  = (11, 3)
     config_Agent.discount           = 0.99
-    config_Agent.lr                 = 1e-4
+    config_Agent.lr                 = 5e-4
     config_Agent.initial_epsilon    = 0.9
     config_Agent.epsilon            = config_Agent.initial_epsilon
     config_Agent.epsilon_decay      = 0.9
     config_Agent.epsilon_min        = 0.02
     config_Agent.steps_to_eps_min   = 5000
-    config_Agent.batch_size         = 2
+    config_Agent.batch_size         = 512
     config_Agent.batches_number     = 10
-    config_Agent.min_memory_size    = 258
+    config_Agent.min_memory_size    = 1000
     config_Agent.memory_size        = 10000
     config_Agent.num_workers        = 0 # set it to 0 if your computer can't handle multiprocessing
     
